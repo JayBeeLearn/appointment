a6cbe96941c3bb65f35cc88bd5b30cfb9d8ec0aa
Jahaziel Uko <jaybeelearn@gmail.com>
2022-08-15 17:17:59 +0100

    change the DesignBy link to open in a new tab

--- resources/views/include/designby.blade.php
+++ resources/views/include/designby.blade.php
@@ -1,6 +1,6 @@
   <footer class="footer">
         <div class="container d-flex-justify-content-center">
               <div class="signature" >Designed by yours truly <span class="design">
-                <a href="https://www.facebook.com/jahaziel.uko.1">JayBee 2021</a>  </span> </div>
+                <a href="https://www.facebook.com/jahaziel.uko.1" target="_blank">JayBee 2021</a>  </span> </div>
           </div>
     </footer>
