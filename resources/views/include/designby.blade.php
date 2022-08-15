6ef73da52b5033866189053f5139a69f50f7cc88
Jahaziel Uko <jaybeelearn@gmail.com>
2022-08-15 18:03:01 +0100

    DesignBy fixed permanently at the bottom of the page

--- resources/views/include/designby.blade.php
+++ resources/views/include/designby.blade.php
@@ -1,6 +1,6 @@
   <footer class="footer">
         <div class="container d-flex-justify-content-center">
-              <div class="signature" >Designed by yours truly <span class="design">
+              <div class="signature" >Designed by yours truly <span class="btn design">
                 <a href="https://www.facebook.com/jahaziel.uko.1" target="_blank">JayBee 2021</a>  </span> </div>
           </div>
     </footer>
